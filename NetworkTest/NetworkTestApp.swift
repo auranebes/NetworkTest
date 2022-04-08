0fd4f39c9633dff73e4550b8959b7af739ee4eef
Arslan Abdullaev <ars352@yahoo.com>
2022-04-08 21:22:47 +0300

    Initial Commit

---
+++ NetworkTest/NetworkTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkTestApp.swift
+//  NetworkTest
+//
+//  Created by Arslan Abdullaev on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
